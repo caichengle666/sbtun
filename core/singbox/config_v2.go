ba040fcb1fb4e58190b44a9ef6967e780f1ac8ab
caichengle666 <276177229+caichengle666@users.noreply.github.com>
2026-09-11 09:12:08 +0800

    feat: 固化智能分流规则集来源

---
+++ core/singbox/config_v2.go
@@ -0,0 +1,7 @@
+package singbox
+
+// SmartRoutingRuleSetURL 提供智能分流使用的官方规则集地址。
+const (
+	SmartGeositeCNURL = "https://raw.githubusercontent.com/SagerNet/sing-geosite/rule-set/geosite-cn.srs"
+	SmartGeoIPCNURL   = "https://raw.githubusercontent.com/SagerNet/sing-geoip/rule-set/geoip-cn.srs"
+)
