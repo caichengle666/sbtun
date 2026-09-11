363399aef1b5084005056b89557f663c2441418d
caichengle666 <276177229+caichengle666@users.noreply.github.com>
2026-09-11 09:19:36 +0800

    fix: align Wails v2.15 application option types

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"github.com/caichengle666/sbtun/app"
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
+	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+	"github.com/wailsapp/wails/v2/pkg/options/windows"
 )
 
 //go:embed frontend/dist
@@ -15,16 +17,18 @@ func main() {
 	application := app.New()
 
 	err := wails.Run(&options.App{
-		Title:             "sbtun",
-		Width:             980,
-		Height:            680,
-		MinWidth:          760,
-		MinHeight:         520,
-		BackgroundColour:  &options.RGBA{R: 13, G: 14, B: 21, A: 1},
-		AssetServer:       options.AssetServer{Assets: assets},
-		OnStartup:         application.Startup,
-		Bind:              []interface{}{application},
-		Windows: options.Windows{
+		Title:            "sbtun",
+		Width:            980,
+		Height:           680,
+		MinWidth:         760,
+		MinHeight:        520,
+		BackgroundColour: &options.RGBA{R: 13, G: 14, B: 21, A: 255},
+		AssetServer: &assetserver.Options{
+			Assets: assets,
+		},
+		OnStartup: application.Startup,
+		Bind:      []interface{}{application},
+		Windows: &windows.Options{
 			WebviewIsTransparent: false,
 			WindowIsTranslucent:  false,
 			DisableWindowIcon:   false,
