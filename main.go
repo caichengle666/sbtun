2d2b4961f4910ae8f544ab733a723ec362f440ef
caichengle666 <276177229+caichengle666@users.noreply.github.com>
2026-09-11 09:18:05 +0800

    build: switch executable to Wails desktop shell

--- main.go
+++ main.go
@@ -1,11 +1,36 @@
 package main
 
 import (
-	"fmt"
-	"runtime"
+	"embed"
+
+	"github.com/caichengle666/sbtun/app"
+	"github.com/wailsapp/wails/v2"
+	"github.com/wailsapp/wails/v2/pkg/options"
 )
 
+//go:embed frontend/dist
+var assets embed.FS
+
 func main() {
-	fmt.Printf("sbtun - 轻量级 sing-box TUN 客户端\n")
-	fmt.Printf("平台: %s/%s\n", runtime.GOOS, runtime.GOARCH)
+	application := app.New()
+
+	err := wails.Run(&options.App{
+		Title:             "sbtun",
+		Width:             980,
+		Height:            680,
+		MinWidth:          760,
+		MinHeight:         520,
+		BackgroundColour:  &options.RGBA{R: 13, G: 14, B: 21, A: 1},
+		AssetServer:       options.AssetServer{Assets: assets},
+		OnStartup:         application.Startup,
+		Bind:              []interface{}{application},
+		Windows: options.Windows{
+			WebviewIsTransparent: false,
+			WindowIsTranslucent:  false,
+			DisableWindowIcon:   false,
+		},
+	})
+	if err != nil {
+		panic(err)
+	}
 }
