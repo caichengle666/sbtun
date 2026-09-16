f73ae9652397f5885bdc4529f3fb8766af329a3c
zhumao <caichengle@gmail.com>
2026-09-16 14:05:13 +0800

    release: bump version to 0.1.18

--- app/version.go
+++ app/version.go
@@ -1,6 +1,6 @@
 package app
 
-const version = "0.1.17"
+const version = "0.1.18"
 
 // Version returns the application version used by CLI and diagnostics.
 func Version() string { return version }
